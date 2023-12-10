f4e7c6609c955616cb4f48513f93c04f5c46a831
private-yusuke <30387586+private-yusuke@users.noreply.github.com>
2023-12-10 17:43:01 +0900

    Deny ICMP Echo Request to 93.184.216.34 (example.com)

--- c103-tp-aya-sample-ebpf/src/main.rs
+++ c103-tp-aya-sample-ebpf/src/main.rs
@@ -1,8 +1,16 @@
 #![no_std]
 #![no_main]
 
-use aya_bpf::{macros::classifier, programs::TcContext};
+use aya_bpf::{
+    bindings::{TC_ACT_SHOT, TC_ACT_UNSPEC},
+    macros::classifier,
+    programs::TcContext,
+};
 use aya_log_ebpf::info;
+use network_types::{
+    eth::{EthHdr, EtherType},
+    ip::{IpProto, Ipv4Hdr},
+};
 
 #[classifier]
 pub fn c103_tp_aya_sample(ctx: TcContext) -> i32 {
@@ -13,8 +21,22 @@ pub fn c103_tp_aya_sample(ctx: TcContext) -> i32 {
 }
 
 fn try_c103_tp_aya_sample(ctx: TcContext) -> Result<i32, i32> {
-    info!(&ctx, "received a packet");
-    Ok(0)
+    // IPv4 のパケット以外は処理しない
+    let ethhdr: EthHdr = ctx.load(0).map_err(|_| TC_ACT_UNSPEC)?;
+    if !matches!(ethhdr.ether_type, EtherType::Ipv4) {
+        return Ok(TC_ACT_UNSPEC);
+    }
+    // ICMP のパケット以外は処理しない
+    let ipv4hdr: Ipv4Hdr = ctx.load(EthHdr::LEN).map_err(|_| TC_ACT_UNSPEC)?;
+    if ipv4hdr.proto != IpProto::Icmp {
+        return Ok(TC_ACT_UNSPEC);
+    }
+    // ICMP のパケットのうち、宛先が 93.184.216.34 のものはドロップする
+    if ipv4hdr.dst_addr.to_be() == 0x5db8d822 {
+        info!(&ctx, "ICMP packet to 93.184.216.34 is dropped");
+        return Ok(TC_ACT_SHOT);
+    }
+    Ok(TC_ACT_UNSPEC)
 }
 
 #[panic_handler]
